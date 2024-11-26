84af7f247229a864bb59640d35888b261b8eb7bd
Angel0Garcia <gabrielg080nac@gmail.com>
2024-11-26 09:36:39 -0400

    Update Program.cs

--- JardinBontanico/Program.cs
+++ JardinBontanico/Program.cs
@@ -28,7 +28,7 @@ namespace JardinBontanico
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=SubirImagen}/{id?}");
 
             app.Run();
         }
